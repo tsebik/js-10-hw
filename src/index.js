def5eb7363f5854b032af9628df278e741e3ac94
Дмитрий Цебинога <tsebik@ukr.net>
2022-08-06 15:37:17 +0300

    making hw-10

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
+import Notiflix from 'notiflix';
 const debounce = require('lodash.debounce');
 
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-function findCountry(country) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${country}?fields=name.official,capital,population,flags.svg,languages`
-  ).then(response => response.json());
-}
-
-findCountry('Ukrain').then(country => {
-  console.log(country);
-});
-
 const searchBox = document.querySelector('#search-box');
+const ul = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
 
 searchBox.addEventListener('input', debounce(onInputEnter, DEBOUNCE_DELAY));
 
+function findCountry(countries) {
+  return fetch(
+    `https://restcountries.com/v2/name/${countries}?fields=flags,name,capital,population,languages`
+  ).then(response => response.json());
+}
+
 function onInputEnter(e) {
-  const inputValue = e.target.value;
+  findCountry(e.target.value.trim()).then(country => {
+    makeCountryMarkup(country);
+  });
+}
+
+function makeCountryMarkup(country) {
+  if (country.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
 }
